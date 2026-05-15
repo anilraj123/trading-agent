66d9e29ca787c456570fc388e466f261c2eec637
Anil <anil@example.com>
2026-05-14 19:02:15 -0700

    Remove files containing personal info (passwords, IPs, home paths)

--- pull-data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Pull bot data from DigitalOcean droplet to local data/ directory
-sshpass -p '[REDACTED]' scp -o StrictHostKeyChecking=no -r root@[REDACTED_IP]:/root/trading-agent/data/ ./data/
-echo "Data pulled to ./data/"
-ls -la ./data/
